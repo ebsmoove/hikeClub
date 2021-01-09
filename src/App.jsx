c023bf3de4f7b44373050b0307db3381152115ec
hs-eboshart <eric@homestars.com>
2021-01-09 09:27:04 -0500

    reactified file names

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { api, mockApiResponse } from "./services/";
+import { api, mockApiResponse } from "./services";
 import {
   Header,
   TrailAttributesForm,
