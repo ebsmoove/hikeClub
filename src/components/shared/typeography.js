3418184b7579906fc56038d072cd5775b6b4927a
hs-eboshart <eric@homestars.com>
2021-01-10 16:28:35 -0500

    added px values

--- src/components/shared/typeography.js
+++ src/components/shared/typeography.js
@@ -3,43 +3,44 @@ import styled from "styled-components";
 
 export const H1 = styled.h1`
   font-family: "Roboto", sans-serif;
-  font-size: 96;
+  font-size: 96px;
+  margin: 0;
 `;
 
 export const H2 = styled.h2`
   font-family: "Roboto", sans-serif;
-  font-size: 60;
+  font-size: 60px;
 `;
 
 export const H3 = styled.h2`
-  font-size: 48;
+  font-size: 48px;
 `;
 
 export const B1 = styled.p`
   font-family: "Roboto", sans-serif;
-  font-size: 16;
+  font-size: 16px;
 `;
 
 export const B2 = styled.p`
   font-family: "Roboto", sans-serif;
-  font-size: 14;
+  font-size: 14px;
 `;
 
 export const ST1 = styled.p`
   font-family: "Roboto", sans-serif;
-  font-size: 16;
+  font-size: 16px;
   font-weight: bold;
 `;
 
 export const ST2 = styled.p`
   font-family: "Roboto", sans-serif;
-  font-size: 14;
+  font-size: 14px;
   font-style: italic;
 `;
 
 export const BUTT1 = styled.p`
   font-family: "Roboto", sans-serif;
-  font-size: 14;
+  font-size: 14px;
   font-weight: bold;
   margin: 0;
   text-transform: uppercase;
