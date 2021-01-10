03f62d694bf3d7c22c932c9178cde8b3f546409c
hs-eboshart <eric@homestars.com>
2021-01-10 11:39:39 -0500

    simple fold animation

--- src/components/trailAttributesForm/index.jsx
+++ src/components/trailAttributesForm/index.jsx
@@ -1,6 +1,6 @@
+import React, { useState } from "react";
 import { Formik, Form, Field } from "formik";
 import styled from "styled-components";
-import React from "react";
 import PropTypes from "prop-types";
 import { TextField } from "formik-material-ui";
 import RadioGroup from "./RadioGroup";
@@ -8,12 +8,19 @@ import SubmitButton from "./SubmitButton";
 import { trailAttritubeFormEnums } from "../../enums";
 import { api } from "../../services";
 
-const StyledForm = styled(Form)`
-  display: flex;
-  flex-direction: column;
-  padding: 15px;
-  > * {
-    margin: 10px;
+const TrailAttributesFormContainer = styled.div``;
+
+const FormContainer = styled.div`
+  height: ${({ isVisible }) => (isVisible ? "100%" : "0px")};
+  transition: height 200ms;
+  overflow: hidden;
+  form {
+    display: flex;
+    flex-direction: column;
+    padding: 15px;
+    > * {
+      margin: 10px;
+    }
   }
 `;
 
@@ -21,53 +28,68 @@ const RadioGroups = styled.div`
   display: flex;
 `;
 
-const TrailAttributesForm = ({ setTrails }) => (
-  <Formik
-    initialValues={{
-      [trailAttritubeFormEnums.MIN_LENGTH]: 10,
-      [trailAttritubeFormEnums.MAX_LENGTH]: 100,
-      [trailAttritubeFormEnums.OTC_TRAIL_CHALLENGE]: "Challenging",
-      [trailAttritubeFormEnums.ACTIVITY]: "Both",
-    }}
-    onSubmit={async (values) => {
-      const getLioMapServerResponse = await api.getLioMapServer(values);
-      setTrails(getLioMapServerResponse);
-    }}
-  >
-    {({ isSubmitting }) => (
-      <StyledForm>
-        <Field
-          name={trailAttritubeFormEnums.MIN_LENGTH}
-          label={trailAttritubeFormEnums.MIN_LENGTH_LABEL}
-          type="number"
-          component={TextField}
-        />
-        <Field
-          name={trailAttritubeFormEnums.MAX_LENGTH}
-          label={trailAttritubeFormEnums.MAX_LENGTH_LABEL}
-          type="number"
-          component={TextField}
-        />
-        <RadioGroups>
-          <RadioGroup
-            labelText={trailAttritubeFormEnums.OTC_TRAIL_CHALLENGE_LABEL}
-            name={trailAttritubeFormEnums.OTC_TRAIL_CHALLENGE}
-            values={trailAttritubeFormEnums.OTC_TRAIL_CHALLENGE_VALUES}
-            isSubmitting={isSubmitting}
-          />
-          <RadioGroup
-            labelText={trailAttritubeFormEnums.ACTIVITY_LABEL}
-            name={trailAttritubeFormEnums.ACTIVITY}
-            values={trailAttritubeFormEnums.ACTIVITY_VALUES}
-            isSubmitting={isSubmitting}
-          />
-        </RadioGroups>
+const TrailAttributesForm = ({ setTrails }) => {
+  const [isVisible, setIsVisible] = useState(false);
+  return (
+    <TrailAttributesFormContainer>
+      <button
+        type="button"
+        onClick={() => {
+          setIsVisible(!isVisible);
+        }}
+      >
+        FOLD
+      </button>
+      <FormContainer isVisible={isVisible}>
+        <Formik
+          initialValues={{
+            [trailAttritubeFormEnums.MIN_LENGTH]: 10,
+            [trailAttritubeFormEnums.MAX_LENGTH]: 100,
+            [trailAttritubeFormEnums.OTC_TRAIL_CHALLENGE]: "Challenging",
+            [trailAttritubeFormEnums.ACTIVITY]: "Both",
+          }}
+          onSubmit={async (values) => {
+            const getLioMapServerResponse = await api.getLioMapServer(values);
+            setTrails(getLioMapServerResponse);
+          }}
+        >
+          {({ isSubmitting }) => (
+            <Form>
+              <Field
+                name={trailAttritubeFormEnums.MIN_LENGTH}
+                label={trailAttritubeFormEnums.MIN_LENGTH_LABEL}
+                type="number"
+                component={TextField}
+              />
+              <Field
+                name={trailAttritubeFormEnums.MAX_LENGTH}
+                label={trailAttritubeFormEnums.MAX_LENGTH_LABEL}
+                type="number"
+                component={TextField}
+              />
+              <RadioGroups>
+                <RadioGroup
+                  labelText={trailAttritubeFormEnums.OTC_TRAIL_CHALLENGE_LABEL}
+                  name={trailAttritubeFormEnums.OTC_TRAIL_CHALLENGE}
+                  values={trailAttritubeFormEnums.OTC_TRAIL_CHALLENGE_VALUES}
+                  isSubmitting={isSubmitting}
+                />
+                <RadioGroup
+                  labelText={trailAttritubeFormEnums.ACTIVITY_LABEL}
+                  name={trailAttritubeFormEnums.ACTIVITY}
+                  values={trailAttritubeFormEnums.ACTIVITY_VALUES}
+                  isSubmitting={isSubmitting}
+                />
+              </RadioGroups>
 
-        <SubmitButton type="submit" isSubmitting={isSubmitting} />
-      </StyledForm>
-    )}
-  </Formik>
-);
+              <SubmitButton type="submit" isSubmitting={isSubmitting} />
+            </Form>
+          )}
+        </Formik>
+      </FormContainer>
+    </TrailAttributesFormContainer>
+  );
+};
 
 TrailAttributesForm.propTypes = {
   setTrails: PropTypes.func.isRequired,
